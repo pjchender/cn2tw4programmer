546404267b34e63f3b1b6cf4573560d4d76894b8
Ann <extend1994@gmail.com>
2018-09-16 16:20:14 +0800

    Add some words & phrases for better translation (#2)
    Thanks for your contribution!

--- src/terms/zh_TW.js
+++ src/terms/zh_TW.js
@@ -1,9 +1,10 @@
 // 繁中
-const sortedObjectByKey = require('./../utilities/sortObjectByKey');
+// const sortedObjectByKey = require('./../utilities/sortObjectByKey');
 
 const zh_TW = {
   phrases: {
     'assembly language': '組合語言',
+    'cloud computing': '雲端運算',
     'connection-oriented': '連結導向',
     'constructor function': '建構式函式',
     'domain name': '網域名稱',
@@ -11,42 +12,74 @@ const zh_TW = {
     'low level language': '低階語言',
     'object-oriented': '物件導向',
     'plan text': '純文字',
-    'production mode': '正式模式'
+    'production mode': '正式模式',
+    'source code': '原始碼'
   },
   words: {
     array: '陣列',
     asynchronous: '非同步',
     attribute: '屬性',
+    bandwidth: '頻寬',
+    blog: '部落格',
     byte: '位元組',
+    cache: '快取',
     call: '呼叫',
+    character: '字元',
+    client: '用戶端',
+    cluster: '叢集',
     code: '程式碼',
+    command: '指令',
+    community: '社群',
+    compatible: '相容',
+    data: '資料',
     debug: '偵錯',
     default: '預設值',
+    document: '文件',
     export: '匯出',
+    extension: '副檔名',
+    file: '檔案',
+    folder: '資料夾',
     gateway: '閘道器',
+    homepage: '首頁',
     import: '匯入',
     inline: '行內',
     internet: '網際網路',
+    lan: '區域網路',
     list: '清單',
     load: '載入',
+    login: '登入',
     memory: '記憶體',
     module: '模組',
+    motherboard: '主機板',
+    mouse: '滑鼠',
+    network: '網路',
     object: '物件',
+    online: '線上',
+    packet: '封包',
     port: '連接埠',
+    post: '文章',
     process: '程序',
+    program: '程式',
     programmer: '工程師',
+    programming: '程式設計',
     protocol: '協定',
     recursion: '遞迴',
+    refresh: '重新整理',
     register: '暫存器',
     response: '回應',
+    return: '回傳',
+    scheduling: '排程',
     screen: '螢幕',
     server: '伺服器',
+    setting: '設定',
     stack: '堆疊',
+    submit: '送出',
     thread: '執行緒',
+    uninstall: '解除安裝',
     variable: '變數',
     window: '視窗'
   }
-}
+};
 
 // console.log(sortedObjectByKey(zh_TW));
 

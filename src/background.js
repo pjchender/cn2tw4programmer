4e9338d534f36037b93eb8d95e3366e1069d4e34
pjchender <pjchender@gmail.com>
2020-04-13 10:48:16 +0800

    fix: remove permissions of declarative content

--- src/background.js
+++ src/background.js
@@ -1,27 +1,5 @@
 /* global chrome */
 
-// When the extension is installed or upgraded ...
-chrome.runtime.onInstalled.addListener(function() {
-  // Replace all rules ...
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    // With a new rule ...
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        // That fires when a page's URL matches one of the following ...
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {
-              schemes: ['https', 'http']
-            }
-          })
-        ],
-        // And shows the extension's page action.
-        actions: [new chrome.declarativeContent.ShowPageAction()]
-      }
-    ]);
-  });
-});
-
-chrome.pageAction.onClicked.addListener(function() {
+chrome.browserAction.onClicked.addListener(function() {
   chrome.tabs.executeScript({ file: 'contentScript.bundle.js' });
 });
